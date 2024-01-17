504ce5d5d4538a50b841591188f3ac01f13d374e
Justin Beausoleil <beausoleil.justin@gmail.com>
2024-01-16 17:03:37 -0700

    Adding funchtions to investment calculator.

--- go-basics/investment_calculator.go
+++ go-basics/investment_calculator.go
@@ -5,8 +5,9 @@ import (
 	"math"
 )
 
+const inflationRate = 6.5
+
 func main() {
-	const inflationRate = 6.5
 	// var investmentAmount = 1000  			// integer
 	//var investmentAmount float64 = 1000 		// explicitly called float64
 	var investmentAmount float64 // go expects type of return
@@ -15,22 +16,31 @@ func main() {
 	//var years float64 = 10
 	var years float64
 
-	fmt.Print("Investment amount: ")
+	outputText("Investment amount: ")
 	fmt.Scan(&investmentAmount)
 
-	fmt.Print("Expected return rate: ")
+	outputText("Expected return rate: ")
 	fmt.Scan(&expectedReturnRate)
 
-	fmt.Print("Years: ")
+	outputText("Years: ")
 	fmt.Scan(&years)
 
-	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
-	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+	fv, frv := calculateFutureValues(investmentAmount, expectedReturnRate, years)
 	//fmt.Println("Future Value:", futureValue)
-	fmt.Printf("Future Value: %.2f\n", futureValue)
-	fmt.Printf("Future Adjusted Value: %.2f", futureRealValue)
+	fmt.Printf("Future Value: %.2f\n", fv)
+	fmt.Printf("Future Adjusted Value: %.2f", frv)
 	//fmt.Printf("Future Value: %.2f\n"+
 	//	"Future Value: %.2f\n",
 	//	futureValue,
 	//	futureRealValue)
+}
+
+func outputText(text string) {
+	fmt.Print(text)
+}
+
+func calculateFutureValues(investmentAmount, expectedReturnRate, years float64) (float64, float64) {
+	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+	return futureValue, futureRealValue
 }
