165a4dc7f2533b732ba95fc8e90b7327f0ac8eb4
Justin Beausoleil <beausoleil.justin@gmail.com>
2024-01-16 20:58:12 -0700

    Adding in read / write file for bank application.

--- go-bank/bank.go
+++ go-bank/bank.go
@@ -1,13 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const accountBalanceFile = "balance.txt"
+
+func getBalanceFromFile() float64 {
+	data, _ := os.ReadFile(accountBalanceFile)
+	balanceText := string(data)
+	balance, _ := strconv.ParseFloat(balanceText, 64)
+	return balance
+}
+
+func writeBalanceToFile(balance float64) {
+	balanceText := fmt.Sprint(balance)
+	err := os.WriteFile(accountBalanceFile, []byte(balanceText), 0644)
+	if err != nil {
+		return
+	}
+}
 
 func main() {
-	var accountBalance float64 = 1000
+	var accountBalance = getBalanceFromFile()
 
 	fmt.Println("Welcome to Go Bank!")
 
-	//for i := 0; i < 2; i++ {
 	for {
 		fmt.Println("What do you want to do?")
 
@@ -20,9 +40,11 @@ func main() {
 		fmt.Print("Your choice: ")
 		fmt.Scan(&choice)
 
-		if choice == 1 {
+		switch choice {
+		case 1:
 			fmt.Println("Your account balance is:", accountBalance)
-		} else if choice == 2 {
+
+		case 2:
 			fmt.Print("Your deposit: ")
 			var depositAmount float64
 			fmt.Scan(&depositAmount)
@@ -35,7 +57,8 @@ func main() {
 
 			accountBalance += depositAmount
 			fmt.Println("Balance updated.  Your account balance is now:", accountBalance)
-		} else if choice == 3 {
+			writeBalanceToFile(accountBalance)
+		case 3:
 			fmt.Print("Withdrawal amount: ")
 			var withdrawlAmount float64
 			fmt.Scan(&withdrawlAmount)
@@ -52,13 +75,13 @@ func main() {
 
 			accountBalance -= withdrawlAmount
 			fmt.Println("Balance updated.  Your account balance is now:", accountBalance)
-		} else {
+			writeBalanceToFile(accountBalance)
+		default:
 			fmt.Println("Goodbye.")
-			//return // will not allow code outside of loop to be executed
-			break // exit program - will allow outside of loop to be executed
+			fmt.Println("Thanks for choosing our bank")
+			return // will not allow code outside of loop to be executed - must be used for switch statements
+			//break // exit program - will allow outside of loop to be executed
 		}
-
-		//fmt.Println("Your choice: ", choice)
+		fmt.Println("----------------------------------------")
 	}
-	fmt.Println("Thanks for choosing our bank")
 }
