6735f9165df90951cc1d5c9843cc608bf464aefa
Justin Beausoleil <justin.beausoleil@justin.beausoleil-XTVDYHJ64W>
2024-01-19 13:18:09 -0500

    Adding error handling for profit_calculator and bank application.

--- go-profit-calculator/profit_calculator.go
+++ go-profit-calculator/profit_calculator.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"os"
 )
 
 // Goals
@@ -13,27 +15,42 @@ import (
 func main() {
 
 	// Ask for revenue, expenses and tax rate
-	revenue := getUserInput("Revenue: ")
-	expenses := getUserInput("Expenses: ")
-	taxRate := getUserInput("Tax Rate: ")
+	revenue, err01 := getUserInput("Revenue: ")
+	expenses, err02 := getUserInput("Expenses: ")
+	taxRate, err03 := getUserInput("Tax Rate: ")
+
+	if err01 != nil || err02 != nil || err03 != nil {
+		fmt.Println("Error: ", err03)
+		return
+	}
 
 	earningsBeforeTax, profit, expenseRatio := calculateFinancials(revenue, expenses, taxRate)
+	storeResults(earningsBeforeTax, profit, expenseRatio)
 
 	fmt.Printf("Earnings before tax: %.2f\n", earningsBeforeTax)
 	fmt.Printf("Profit: %.2f\n", profit)
 	fmt.Printf("Expense Ratio: %.2f", expenseRatio)
 }
 
-func getUserInput(infoText string) float64 {
+func getUserInput(infoText string) (float64, error) {
 	var userInput float64
-
 	fmt.Print(infoText)
-	_, err := fmt.Scan(&userInput)
-	if err != nil {
-		return 0
+	fmt.Scan(&userInput)
+
+	// Validate user input and return error if negative or zero
+	if userInput <= 0 {
+		return 0, errors.New("value cannot be negative or zero")
 	}
 
-	return userInput
+	return userInput, nil
+}
+
+func storeResults(ebt, profit, ratio float64) {
+	results := fmt.Sprintf("EBT: %.2f\nProfit: %.2f\nRatio: %.2f\n", ebt, profit, ratio)
+	err := os.WriteFile("results.txt", []byte(results), 0644)
+	if err != nil {
+		return
+	}
 }
 
 func calculateFinancials(revenue, expenses, taxRate float64) (float64, float64, float64) {
